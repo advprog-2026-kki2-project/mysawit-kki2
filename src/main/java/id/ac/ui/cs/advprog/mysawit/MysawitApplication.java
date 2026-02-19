fd94a7d6fc3fefe39ac5a6139bff4e3a404859e2
Davin Muhammad Hijran <davinmagic3@gmail.com>
2026-02-19 13:05:30 +0700

    Initialization of mysawit

---
+++ src/main/java/id/ac/ui/cs/advprog/mysawit/MysawitApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.mysawit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysawitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysawitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/mysawit/MysawitApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.mysawit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysawitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
