bbf27aa904abd647eae7c9b92e2009ec9c6bc8ff
kerangleeya <shelia.vellicita13@gmail.com>
2026-04-16 23:49:31 +0700

    feat(payment): creating wage configuration repository

---
+++ src/main/java/id/ac/ui/cs/advprog/mysawit/modules/payment/repository/WageConfigurationRepository.java
@@ -0,0 +1,7 @@
+package id.ac.ui.cs.advprog.mysawit.modules.payment.repository;
+
+import id.ac.ui.cs.advprog.mysawit.modules.payment.model.WageConfiguration;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WageConfigurationRepository extends JpaRepository<WageConfiguration, Long> {
+}
