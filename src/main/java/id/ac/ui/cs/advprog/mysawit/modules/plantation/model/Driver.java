512e78c5a71b8cd1979e63b74011e78ff6e50dfc
zahran-musyaffa <zahran.musyaffa@ui.ac.id>
2026-04-16 21:12:17 +0700

    add driver mode

---
+++ src/main/java/id/ac/ui/cs/advprog/mysawit/modules/plantation/model/Driver.java
@@ -0,0 +1,16 @@
+package id.ac.ui.cs.advprog.mysawit.modules.plantation.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Driver {
+
+    private String driverId;
+
+    private String driverName;
+
+    private String licenseNumber;
+
+}
--- src/main/java/id/ac/ui/cs/advprog/mysawit/modules/plantation/model/Plantation.java
+++ src/main/java/id/ac/ui/cs/advprog/mysawit/modules/plantation/model/Plantation.java
@@ -3,6 +3,7 @@ package id.ac.ui.cs.advprog.mysawit.modules.plantation.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -19,5 +20,9 @@ public class Plantation {
     private double areaHectares;
 
     private List<Coordinate> corners;
+
+    private List<String> assignedForemanIds = new ArrayList<>();
+
+    private List<String> assignedDriverIds = new ArrayList<>();
 
 }
