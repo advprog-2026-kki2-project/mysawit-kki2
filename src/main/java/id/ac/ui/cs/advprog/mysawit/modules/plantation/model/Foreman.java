e821f10cf4d7fd2780f6017162305bed3a8d955d
zahran-musyaffa <zahran.musyaffa@ui.ac.id>
2026-04-16 22:22:47 +0700

    foremen model

---
+++ src/main/java/id/ac/ui/cs/advprog/mysawit/modules/plantation/model/Foreman.java
@@ -0,0 +1,16 @@
+package id.ac.ui.cs.advprog.mysawit.modules.plantation.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Foreman {
+
+    private String foremanId;
+
+    private String foremanName;
+
+    private String employeeCode;
+
+}
